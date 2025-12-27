ac11bd12d4bb7f9a3d04d9b761894fa25371d011
Rody Huancas <rodyhuancas.04@gmail.com>
2025-12-26 21:47:57 -0500

    Feat: Refactor ProductsApp component to integrate ApplicationProvider and IconRegistry

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -1,12 +1,25 @@
-import "react-native-gesture-handler"
+import 'react-native-gesture-handler';
+import * as eva from '@eva-design/eva';
 
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from "./presentation/navigation/StackNavigator";
+import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import { StackNavigator } from './presentation/navigation/StackNavigator';
 
 export const ProductsApp = () => {
+  const colorScheme = useColorScheme();
+  const theme       = colorScheme === 'dark' ? eva.dark : eva.light;
+
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <>
+      <ApplicationProvider {...eva} theme={theme}>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </ApplicationProvider>
+
+      <IconRegistry icons={EvaIconsPack} />
+    </>
   );
 };
