4ed823cec62ce9b989ff34e9b74fe9d178ab15cf
Rody Huancas <rodyhuancas.04@gmail.com>
2025-12-26 21:48:15 -0500

    Feat: Implement LoginScreen with input fields and button for user authentication

--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -1,9 +1,68 @@
-import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useWindowDimensions } from 'react-native';
+import { Button, Input, Layout, Text } from '@ui-kitten/components';
+import { MyIcon } from '../../components/ui/MyIcon';
 
 export const LoginScreen = () => {
+  const { height } = useWindowDimensions();
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <Layout style={{ flex: 1 }}>
+      <ScrollView style={{ marginHorizontal: 40 }}>
+        <Layout style={{ paddingTop: height * 0.35 }}>
+          <Text category="h1">Ingresar</Text>
+          <Text category="p2">Por favor, ingresa para continuar</Text>
+        </Layout>
+
+        {/* Inputs */}
+        <Layout style={{ marginTop: 20 }}>
+          <Input
+            placeholder="Correo electrónico"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            accessoryLeft={<MyIcon name="email-outline" white />}
+            style={{ marginBottom: 10 }}
+          />
+
+          <Input
+            placeholder="Contraseña"
+            autoCapitalize="none"
+            secureTextEntry
+            accessoryLeft={<MyIcon name="lock-outline" white />}
+            style={{ marginBottom: 10 }}
+          />
+        </Layout>
+
+        {/* Space */}
+        <Layout style={{ height: 20 }} />
+
+        {/* Button */}
+        <Layout>
+          <Button
+            accessoryRight={<MyIcon name="arrow-forward-outline" white />}
+            onPress={() => {}}
+          >
+            Ingresar
+          </Button>
+        </Layout>
+
+        {/* Space */}
+        <Layout style={{ height: 50 }} />
+
+        {/* Información para crear cuenta */}
+        <Layout
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text category="p2">¿No tienes una cuenta? </Text>
+          <Text status="primary" category="s1" onPress={() => {}}>
+            Crea una
+          </Text>
+        </Layout>
+      </ScrollView>
+    </Layout>
   );
 };
