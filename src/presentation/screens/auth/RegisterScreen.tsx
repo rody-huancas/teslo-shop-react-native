7e86377641feccd3f384029b9b567f729c92f64e
Rody Huancas <rodyhuancas.04@gmail.com>
2025-12-27 21:33:52 -0500

    Refactor: Revamp RegisterScreen layout with improved input fields and navigation

--- src/presentation/screens/auth/RegisterScreen.tsx
+++ src/presentation/screens/auth/RegisterScreen.tsx
@@ -1,9 +1,85 @@
-import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useWindowDimensions } from 'react-native';
+import { Button, Input, Layout, Text } from '@ui-kitten/components';
+import { MyIcon } from '../../components/ui/MyIcon';
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParams } from '../../navigation/StackNavigator';
 
-export const RegisterScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'RegisterScreen'> {}
+
+export const RegisterScreen = ({ navigation }: Props) => {
+  const { height } = useWindowDimensions();
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <Layout style={{ flex: 1 }}>
+      <ScrollView style={{ marginHorizontal: 40 }}>
+        <Layout style={{ paddingTop: height * 0.3 }}>
+          <Text category="h1">Crear cuenta</Text>
+          <Text category="p2">Por favor, crea una cuenta para continuar</Text>
+        </Layout>
+
+        {/* Inputs */}
+        <Layout style={{ marginTop: 20 }}>
+          <Input
+            placeholder="Nombre completo"
+            accessoryLeft={MyIcon({ name: 'person-outline', white: true })}
+            style={{ marginBottom: 10 }}
+          />
+
+          <Input
+            placeholder="Correo electrónico"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            accessoryLeft={MyIcon({ name: 'email-outline', white: true })}
+            style={{ marginBottom: 10 }}
+          />
+
+          <Input
+            placeholder="Contraseña"
+            autoCapitalize="none"
+            secureTextEntry
+            accessoryLeft={MyIcon({ name: 'lock-outline', white: true })}
+            style={{ marginBottom: 10 }}
+          />
+        </Layout>
+
+        {/* Space */}
+        <Layout style={{ height: 20 }} />
+
+        {/* Button */}
+        <Layout>
+          <Button
+            accessoryRight={MyIcon({
+              name: 'arrow-forward-outline',
+              white: true,
+            })}
+            onPress={() => {}}
+          >
+            Crear
+          </Button>
+        </Layout>
+
+        {/* Space */}
+        <Layout style={{ height: 50 }} />
+
+        {/* Información para crear cuenta */}
+        <Layout
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text category="p2">¿Ya tienes una cuenta? </Text>
+          <Text
+            status="primary"
+            category="s1"
+            onPress={() => navigation.goBack()}
+          >
+            Inicia sesión
+          </Text>
+        </Layout>
+      </ScrollView>
+    </Layout>
   );
 };
