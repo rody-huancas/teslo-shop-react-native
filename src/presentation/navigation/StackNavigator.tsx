26b77faf378668de1c276888d6bb8c2ced362714
Rody Huancas <rodyhuancas.04@gmail.com>
2025-12-27 21:33:40 -0500

    Refactor: Enhance StackNavigator with fade animation for screen transitions

--- src/presentation/navigation/StackNavigator.tsx
+++ src/presentation/navigation/StackNavigator.tsx
@@ -1,4 +1,4 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackCardStyleInterpolator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { LoginScreen } from '../screens/auth/LoginScreen';
 import { ProductScreen } from '../screens/product/ProductScreen';
@@ -15,13 +15,41 @@ export type RootStackParams = {
 
 const Stack = createStackNavigator<RootStackParams>();
 
+const fadeAnimation: StackCardStyleInterpolator = ({ current }) => ({
+  cardStyle: {
+    opacity: current.progress,
+  },
+});
+
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName='LoginScreen' screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    <Stack.Navigator
+      initialRouteName="LoginScreen"
+      screenOptions={{
+        headerShown: false,
+        // cardStyleInterpolator: fadeAnimation,
+      }}
+    >
+      <Stack.Screen
+        options={{ cardStyleInterpolator: fadeAnimation }}
+        name="LoadingScreen"
+        component={LoadingScreen}
+      />
+      <Stack.Screen
+        options={{ cardStyleInterpolator: fadeAnimation }}
+        name="LoginScreen"
+        component={LoginScreen}
+      />
+      <Stack.Screen
+        options={{ cardStyleInterpolator: fadeAnimation }}
+        name="RegisterScreen"
+        component={RegisterScreen}
+      />
+      <Stack.Screen
+        options={{ cardStyleInterpolator: fadeAnimation }}
+        name="HomeScreen"
+        component={HomeScreen}
+      />
       <Stack.Screen name="ProductScreen" component={ProductScreen} />
     </Stack.Navigator>
   );
