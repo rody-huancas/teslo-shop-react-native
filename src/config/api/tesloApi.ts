775958802ad5527462c87e41959fa5edd917ce8b
Rody Huancas <rodyhuancas.04@gmail.com>
2025-12-28 21:56:59 -0500

    Refactor: Create tesloApi for API interactions with dynamic base URL

---
+++ src/config/api/tesloApi.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { Platform } from 'react-native';
+import { STAGE, API_URL as PROD_URL, API_URL_IOS, API_URL_ANDROID } from '@env';
+
+export const API_URL =
+  STAGE === 'production'
+    ? PROD_URL
+    : Platform.OS === 'ios'
+      ? API_URL_IOS
+      : API_URL_ANDROID;
+
+const tesloApi = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+// TODO: Interceptors
+
+export {
+  tesloApi
+}
