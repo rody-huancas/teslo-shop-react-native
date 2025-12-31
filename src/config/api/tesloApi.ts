65b1eecc11e7f755677a1f4204fbb6a018938b3f
Rody Huancas <rodyhuancas.04@gmail.com>
2025-12-31 01:53:40 -0500

    Refactor: Add request interceptor to include authorization token in API calls

--- src/config/api/tesloApi.ts
+++ src/config/api/tesloApi.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 import { STAGE, API_URL as PROD_URL, API_URL_IOS, API_URL_ANDROID } from '@env';
+import { StorageAdapter } from '../adapters/storage-adapter';
 
 export const API_URL =
   STAGE === 'production'
@@ -17,6 +18,19 @@ const tesloApi = axios.create({
 });
 
 // TODO: Interceptors
+
+tesloApi.interceptors.request.use(
+  async (config) => {
+
+    const token = await StorageAdapter.getItem('token');
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  }
+);
 
 export {
   tesloApi
