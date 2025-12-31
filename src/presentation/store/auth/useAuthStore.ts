c1bc20b4053fdbbcdc482e3e61333135ad944d84
Rody Huancas <rodyhuancas.04@gmail.com>
2025-12-31 01:54:13 -0500

    Refactor: Integrate authCheckStatus and authRegister functions into useAuthStore for enhanced authentication management

--- src/presentation/store/auth/useAuthStore.ts
+++ src/presentation/store/auth/useAuthStore.ts
@@ -1,15 +1,18 @@
 import { create } from 'zustand';
 import { User } from '../../../domain/entities/user';
-import { authLogin } from '../../../actions/auth/auth';
 import { AuthStatus } from '../../../infrastructure/interfaces/auth.status';
 import { StorageAdapter } from '../../../config/adapters/storage-adapter';
+import { authCheckStatus, authLogin, authRegister } from '../../../actions/auth/auth';
 
 export interface AuthState {
   status : AuthStatus;
   token ?: string;
   user  ?: User;
 
-  login: (email: string, password: string) => Promise<boolean>;
+  login      : (email: string, password: string) => Promise<boolean>;
+  checkStatus: () => Promise<void>;
+  logout     : () => Promise<void>;
+  register   : (fullName: string, email: string, password: string) => Promise<boolean>;
 }
 
 export const useAuthStore = create<AuthState>()((set, get) => ({
@@ -28,12 +31,40 @@ export const useAuthStore = create<AuthState>()((set, get) => ({
 
     await StorageAdapter.setItem('token', resp.token);
 
-    const storeToken = await StorageAdapter.getItem('token');
-
-    console.log(storeToken)
+    await StorageAdapter.getItem('token');
 
     set({ status: 'authenticated', token: resp.token, user: resp.user });
 
     return true;
   },
+  checkStatus: async () => {
+    const resp = await authCheckStatus();
+
+    if (!resp) {
+      set({ status: 'unauthenticated', user: undefined, token: undefined });
+      return;
+    }
+
+    await StorageAdapter.setItem('token', resp.token);
+
+    set({ status: 'authenticated', user: resp.user, token: resp.token });
+  },
+  logout: async () => {
+    await StorageAdapter.removeItem('token');
+    set({ status: 'unauthenticated', user: undefined, token: undefined });
+  },
+  register: async (fullName: string, email: string, password: string) => {
+    const resp = await authRegister(fullName, email, password);
+
+    if (!resp) {
+      set({ status: 'unauthenticated', user: undefined, token: undefined });
+      return false;
+    }
+
+    await StorageAdapter.setItem('token', resp.token);
+
+    set({ status: 'authenticated', user: resp.user, token: resp.token });
+
+    return true;
+  }
 }));
