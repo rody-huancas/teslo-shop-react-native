3d7a6e6469898009330c4becbe3592f63780160f
Rody Huancas <rodyhuancas.04@gmail.com>
2026-01-03 00:35:11 -0500

    Feat: Implement getProductById function for fetching product details by ID

---
+++ src/actions/products/get-product-by-id.ts
@@ -0,0 +1,14 @@
+import { Product } from '../../domain/entities/product';
+import { tesloApi } from '../../config/api/tesloApi';
+import { TesloProduct } from '../../infrastructure/interfaces/teslo-products.responses';
+import { ProductMapper } from '../../infrastructure/mappers/product.mapper';
+
+export const getProductById = async (id: string): Promise<Product> => {
+  try {
+    const { data } = await tesloApi.get<TesloProduct>(`/products/${id}`);
+    return ProductMapper.tesloProductToEntity(data);
+  } catch (error) {
+    console.log(error);
+    throw new Error(`Error fetching product by ID: ${id}`);
+  }
+};
